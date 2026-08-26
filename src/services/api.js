1c20a7c210c32054ece63178c2430c22add906f3
MariaMorales1234 <mjmorales@uniboyaca.edu.co>
2026-08-26 08:58:57 -0500

    archios para la conexion de la api

---
+++ src/services/api.js
@@ -0,0 +1,24 @@
+import Pokemon from "../models/Pokemon.js";
+ 
+const API_URL = "https://pokeapi.co/api/v2/pokemon/";
+
+export async function fetchPokemon(id) {
+    try {
+        const res = await fetch(API_URL + id);
+        if (!RegExp.ok) throw new Error("No se encontro el Pokemon");
+        const data = await res.json();
+
+        const types = data.types.map(t => t.type.name);
+
+        return new Pokemon(
+            data.id,
+            data.name,
+            types,
+            data.sprites.other["official-artwork"].front_default
+        );
+    }catch (error) {
+        console.error(error);
+        return null;
+    }
+    
+}
